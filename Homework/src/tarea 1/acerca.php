33469fb7c2d346be5743ad0d87a9b86430a5efb4
VictorFDJ <victorfernandez200210@gmail.com>
2025-05-22 02:01:15 -0400

    Olvide agregar el video que le recomendaria pero ahora yo lo he agregado

--- Homework/src/tarea 1/acerca.php
+++ Homework/src/tarea 1/acerca.php
@@ -1,12 +1,19 @@
 <?php require('partes/cabeza.php');?>  
 
 <h2>Acerda De</h2>
-
+<div>
 <h3>Victor Manuel Fernandez de Jesus</h3>
 <img src="pictures/MyPhoto.jpg" alt="Photo not found" width="200" height="200">
 
 
 <a href="https://t.me/Manuel2002a" target="_blank" class="Telegram">Link a telegram</a>
 <a href="https://wa.me/18498841590" target="_blank" class="Telegram">Escríbeme por WhatsApp</a>
+
+<a href="https://youtu.be/dXFDqJLM5uY?si=a7zExPHFu0DtV1uP" class="volver">La leyenda de la espada norte
+    (es un manhwa, no se si le interese esos temas pero si le quiere dar una oportunidad, ya sabe donde dar clik)</a>
+
+
+
 </div>
+
 <?php require('partes/foot.php'); ?>
