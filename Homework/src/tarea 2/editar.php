ba6fbb2ec24d7314de73e2d8a1c8c0f48ae413e5
VictorFDJ <victorfernandez200210@gmail.com>
2025-05-27 22:48:16 -0400

    i've added the part about edit

--- Homework/src/tarea 2/editar.php
+++ Homework/src/tarea 2/editar.php
@@ -1,8 +1,41 @@
-<?php 
+<?php
 
 include("libraries/principal.php");
 
 $obra = new obra();
+
+if ($_POST) {
+    // Cargar los datos del formulario en el objeto obra
+    $obra->codigo = $_POST['codigo'];
+    $obra->foto_url = $_POST['foto_url'];
+    $obra->tipo = $_POST['tipo'];
+    $obra->nombre = $_POST['nombre'];
+    $obra->descripcion = $_POST['descripcion'];
+    $obra->pais = $_POST['pais'];
+    $obra->autor = $_POST['autor'];
+
+    // Aquí podrías guardar el objeto obra en una base de datos o archivo
+    // Por simplicidad, solo mostraremos un mensaje de éxito
+    echo "<div class='alert alert-success'>Obra guardada exitosamente!</div>";
+    if (!is_dir('datos')) {
+        plantilla::aplicar();
+        echo "<div class='alert alert-danger'>Error al crear la carpeta de datos</div>";
+        echo "<a href='index.php' class='btn btn-primary'>Volver</a>";
+        exit();
+    }
+    $ruta = 'datos/' . $obra->codigo . '.json';
+
+    $json = json_encode($obra);
+
+    file_put_contents($ruta, $json);
+
+    plantilla::aplicar();
+    echo "<div class='alert alert-success'>Obra guardada correctamente</div>";
+    echo "<a href='index.php' class='btn btn-primary'>Volver</a>";
+    exit();
+}
+
+
 
 plantilla::aplicar();
 
@@ -25,13 +58,13 @@ plantilla::aplicar();
     <div class="mb-3">
         <label for="tipo" class="form-label">Tipo</label>
         <select class="form-select" name="tipo" id="tipo">
-          <option value="">Seleccione</option>
-          <?php
-          foreach (Datos::Tipos_de_Obras() as $key => $value) {
-              $selected = ($obra->tipo == $key) ? "selected" : "";
-              echo "<option value='$key' $selected>$value</option>";
-          } 
-          ?>        
+            <option value="">Seleccione</option>
+            <?php
+            foreach (Datos::Tipos_de_Obras() as $key => $value) {
+                $selected = ($obra->tipo == $key) ? "selected" : "";
+                echo "<option value='$key' $selected>$value</option>";
+            }
+            ?>
         </select>
     </div>
     <!-- Nombre de la obra -->
@@ -54,5 +87,9 @@ plantilla::aplicar();
         <label for="autor" class="form-label">Autor</label>
         <input type="text" class="form-control" name="autor" id="autor" value="<?php echo $obra->autor; ?>">
     </div>
-    
+
+    <div class="text-center">
+        <button type="submit" class="btn btn-primary">Guardar</button>
+        <a href="index.php" class="btn btn-secondary">Cancelar</a>
+    </div>
 </form>
--- Homework/src/tarea 2/index.php
+++ Homework/src/tarea 2/index.php
@@ -1,30 +1,63 @@
-<?php 
+<?php
+require_once 'libraries/objectos.php';
 include("libraries/plantilla.php");
 plantilla::aplicar();
 ?>
-            <div class="text-end">
-                <a href="editar.php" class="btn btn-primary">Agregar</a>
-            </div>
-            <table class="table table-striped table-bordered table-hover">
-                <thead>
-                    <tr>
-                        <th scope="col">Tipo</th>
-                        <th scope="col">Nombre</th>
-                        <th scope="col">Año</th>
-                        <th scope="col">Pais</th>
-                        <th scope="col">Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Serie</td>
-                        <td>Breaking Bad</td>
-                        <td>2008</td>
-                        <td>USA</td>
-                        <td>
-                            <a href="editar.php?id=1" class="btn btn-warning">Editar</a>
-                            <a href="detalle.php?id=1" class="btn btn-danger">Detalle</a>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+<div class="text-end">
+    <a href="editar.php" class="btn btn-primary">Agregar</a>
+</div>
+<table class="table table-striped table-bordered table-hover">
+    <thead>
+        <tr>
+            <th scope="col">Foto</th>
+            <th scope="col">Tipo</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Año</th>
+            <th scope="col">Pais</th>
+            <th scope="col">Acciones</th>
+        </tr>
+    </thead>
+    <tbody>
+        <!-- Example row 
+        <tr>
+            <td>Serie</td>
+            <td>Breaking Bad</td>
+            <td>2008</td>
+            <td>USA</td>
+            <td>
+                <a href="editar.php?id=1" class="btn btn-warning">Editar</a>
+                <a href="detalle.php?id=1" class="btn btn-danger">Detalle</a>
+            </td>
+        </tr>
+        -->
+       <?php
+if(is_dir(filename: 'datos')){
+
+    $archivos = scandir(directory: 'datos');
+
+    foreach($archivos as $archivo){
+        $ruta = 'datos/' . $archivo;
+        if(is_file(filename: $ruta)){
+
+            $json = file_get_contents(filename: $ruta);
+            $obra = json_decode(json: $json);
+?>
+    <tr>
+        <td><img src="<?= $obra->foto_url ?>" alt="<?= $obra->nombre ?>" width="100"></td>
+        <td><?= Datos::Tipos_de_Obras()[$obra->tipo] ?></td>
+        <td><?= $obra->nombre ?></td>
+        <td><?= $obra->autor ?></td>
+        <td><?= $obra->pais ?></td>
+        <td>
+            <a href="editar.php?id=<?= $obra->codigo ?>" class="btn btn-warning">Editar</a>
+            <a href="detalle.php?id=<?= $obra->codigo ?>" class="btn btn-danger">Detalle</a>
+        </td>
+    </tr>
+<?php
+        }
+    }
+}
+?>
+
+    </tbody>
+</table>
