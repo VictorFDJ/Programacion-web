4e75ea124f4fc4b0179de54bcda2a266cb89c73d
VictorFDJ <victorfernandez200210@gmail.com>
2025-06-22 19:08:05 -0400

    he agregado el index y el footer

---
+++ Homework/src/Tarea 5/index.php
@@ -0,0 +1,7 @@
+<?php include('header.php'); ?>
+  <div class="text-center">
+    <img src="img/foto.jpg" class="rounded-circle mb-4" width="150" alt="Foto de perfil">
+    <h1>Yosa Fernández</h1>
+    <p class="lead">Bienvenido a mi portal web con integración de APIs</p>
+  </div>
+<?php include('footer.php'); ?>
