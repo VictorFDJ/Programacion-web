a8b7b92c3bb4b50650a9da4631012ec9d6380c1d
VictorFDJ <victorfernandez200210@gmail.com>
2025-06-22 19:06:34 -0400

    he agregado el header

---
+++ Homework/src/Tarea 5/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Mi Portal Web</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">Mi Portal</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item"><a class="nav-link" href="acerca.php">Acerca de</a></li>
+        <li class="nav-item"><a class="nav-link" href="api1_genero.php">Predicción de Género</a></li>
+        <li class="nav-item"><a class="nav-link" href="api2_edad.php">Edad</a></li>
+        <li class="nav-item"><a class="nav-link" href="api3_universidades.php">Universidades</a></li>
+        <li class="nav-item"><a class="nav-link" href="api4_clima.php">Clima</a></li>
+        <li class="nav-item"><a class="nav-link" href="api5_pokemon.php">Pokémon</a></li>
+        <li class="nav-item"><a class="nav-link" href="api6_noticias.php">Noticias</a></li>
+        <li class="nav-item"><a class="nav-link" href="api7_monedas.php">Monedas</a></li>
+        <li class="nav-item"><a class="nav-link" href="api8_imagenes.php">Imágenes IA</a></li>
+        <li class="nav-item"><a class="nav-link" href="api9_paises.php">Datos de País</a></li>
+        <li class="nav-item"><a class="nav-link" href="api10_chistes.php">Chistes</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="container">
